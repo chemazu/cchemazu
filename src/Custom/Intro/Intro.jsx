98d38b10c285ce594fd7849e8fd9a95d7bba8c5d
chemazu <chukwuemekachemazu@gmail.com>
2020-07-02 00:26:59 -0700

    first commit

---
+++ src/Custom/Intro/Intro.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Intro.css"
+import NameSvg from './NameSvg'
+
+export default function Intro() {
+    return (
+        <div className="Intro">
+            <NameSvg/>
+        </div>
+    )
+}
---
+++ src/View/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Intro from '../../Custom/Intro/Intro'
+import "./Home.css"
+
+export default function Home() {
+    return (
+        <div className ="Home">
+            <Intro/>
+            <div>
+                Hello i am Chukwuemeka Chemazu
+                a Frontend Developer
+            </div>
+        </div>
+    )
+}
