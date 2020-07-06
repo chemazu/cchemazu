b34ca17f2829e9d9123b98da70358035a89d7660
chemazu <chukwuemekachemazu@gmail.com>
2020-07-06 14:12:51 +0100

    Updates

---
+++ precache-manifest.119163dfa00ccddfb76f5b7487d69e93.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f1da9b1df36fc78fa175db49e2962b8",
+    "url": "/cchemazu/index.html"
+  },
+  {
+    "revision": "c46cc222140731b71da2",
+    "url": "/cchemazu/static/css/main.000494b2.chunk.css"
+  },
+  {
+    "revision": "4068d4a4689f08a11371",
+    "url": "/cchemazu/static/js/2.03fc385b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cchemazu/static/js/2.03fc385b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c46cc222140731b71da2",
+    "url": "/cchemazu/static/js/main.35df2458.chunk.js"
+  },
+  {
+    "revision": "f9a2edae292c87b81cb1",
+    "url": "/cchemazu/static/js/runtime-main.f48920e0.js"
+  },
+  {
+    "revision": "04f1d8728b781908558c8f1442c665ed",
+    "url": "/cchemazu/static/media/instagram.04f1d872.svg"
+  },
+  {
+    "revision": "3c8afea5ee97ad99924808a2ab5fec10",
+    "url": "/cchemazu/static/media/linkedin.3c8afea5.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cchemazu/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0e9b9fd2d80223f58ce79b65c26ef09d",
+    "url": "/cchemazu/static/media/right-arrow.0e9b9fd2.svg"
+  }
+]);
